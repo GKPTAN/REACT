4b60a180403df670aaa204ea9a29ab3fb44f96f9
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-11-23 00:49:34 -0300

    aula 3
    - uso de variáveis e seus valores por meio de interpolação { } em arquivos jsx
    
    - uso de funções dentro de outras funções
    
    - uso de um objeto e suas propriedades em arquivos jsx

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -7,10 +7,32 @@ function MyButton() {
 }
 
 function App() {
+  const name = "Guilherme";
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjRHT6KkoBeEvQRWrDKiOuqkpu1G-4LdkULA&s';
+
+  const user = {
+    nome: "Guilherme",
+    idade: 23
+  };
+
   return (
     <div className="App">
       <h1>Olá React</h1>
       <p>Meu primeiro app</p>
+      <p>olá, {newName}</p>
+      <p>soma: {sum(1, 2)}</p>
+      <img src={url} alt="kimetsu no yaiba" />
+      <p>
+        Nome: Guilherme <br />
+        Idade: {user.idade}
+      </p>
       <MyButton />
     </div>
   );
