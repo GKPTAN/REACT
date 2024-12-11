d3aa2604d62285e377fe474a8217f934de96f3f7
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-12-11 20:09:48 -0300

    aula 11 Typescript
    - aprendendo sobre Type assertion e Typecast

---
+++ typescript/aula11.ts
@@ -0,0 +1,24 @@
+let nvalor:number;
+let svalor:string;
+let uvalor:unknown;
+
+uvalor=10;
+nvalor=10;
+svalor='20';
+
+svalor=nvalor.toString();
+
+//nvalor=Number.parseInt(svalor);
+
+//nvalor=<number><unknown>svalor;
+
+//nvalor=<number>uvalor;
+//svalor=<string>uvalor;
+//svalor += 10;
+
+//console.log(typeof(uvalor));
+//console.log(uvalor);
+console.log(typeof(svalor));
+console.log(svalor);
+//console.log(typeof(svalor));
+//console.log(svalor);
