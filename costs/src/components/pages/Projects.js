afd190104efa91cf9e5406d6f916002a62c98b53
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-12-23 16:56:44 -0300

    aula 28 React
    - Criando o componente de loader

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -5,10 +5,12 @@ import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
 import { useState, useEffect } from "react";
+import Loading from "../layout/Loading";
 
 function Projects() {
 
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
 
     const location = useLocation();
     let message = '';
@@ -17,16 +19,19 @@ function Projects() {
     };
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then((resp) => resp.json())
-        .then((data) => {
-            setProjects(data);
-        })
-        .catch(err => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((resp) => resp.json())
+            .then((data) => {
+                setProjects(data);
+                setRemoveLoading(true);
+            })
+            .catch(err => console.log(err))
+        }, 300)
     }, [])
 
     return (
@@ -46,6 +51,10 @@ function Projects() {
                         key={project.id}
                     />
                 ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     );
