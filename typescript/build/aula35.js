0166540fbfbdd3002e7b260e009d923185eadbc7
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2024-12-24 10:26:17 -0300

    aula 36 Typescript
    - aprendendo sobre desestruturação em Typescript

---
+++ typescript/build/aula35.js
@@ -0,0 +1,18 @@
+"use strict";
+let av = [10, 20, 30, 40];
+let [aa, bb, cc, dd] = av;
+const obj = {
+    cor1: "Azul Escuro",
+    cor2: "Vermelho Escuro",
+    cor3: "Preto",
+    cor4: "Branco"
+};
+let { cor1, cor2, cor3, cor4 } = obj;
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
+console.log(cor1);
+console.log(cor2);
+console.log(cor3);
+console.log(cor4);
