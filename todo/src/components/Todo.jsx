764b0fb5b1aa0b7e5488f45a0c1e5cec866663a4
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2025-04-30 21:00:13 -0300

    atualização
    colocando uma condição em telas menores que 768px para mostrar dois icones:
    
    - icone de confirmação: confirma que a tarefa foi completada.
    
    - icone de reset: desfazer a confirmação anterior.

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,12 +1,22 @@
+import { RiCheckboxCircleLine } from 'react-icons/ri';
+import { CgUndo } from 'react-icons/cg';
+
 const Todo = ({ todo, removeTodo, completeTodo }) => {
+
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+
   return (
     <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
-      <div>
-        <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
+      <div className='options'>
+        {isMobile ? (
+          <button className={`${todo.isCompleted ? "complete" : "incomplete"}`} onClick={() => completeTodo(todo.id)}><CgUndo id='undo' /><RiCheckboxCircleLine id='complete' /></button>
+        ) : (
+          <button className="incomplete" onClick={() => completeTodo(todo.id)}>Completar</button>
+        )}
         <button className="remove" onClick={() => removeTodo(todo.id)}>X</button>
       </div>
     </div>
