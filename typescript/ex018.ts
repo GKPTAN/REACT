4a3702adebea23177d00ae435d16c96d156a81e3
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2026-04-29 21:03:48 -0300

    praticando typescript sobre objetos literais e métodos getter e setter

---
+++ typescript/ex018.ts
@@ -0,0 +1,33 @@
+class DataCenter {
+    private _temp: number = 20;
+
+    constructor() {}
+
+    get temp(): number {
+        return this._temp;
+    };
+
+    set temp(temp: number) {
+        if (temp > 40) {
+            console.log(`[ALERTA] Temperatura de ${temp}°C é crítica! ajuste bloqueado.`);
+        } else if (temp < 10) {
+            console.log(`[ALERTA] Temperatura muito baixa! ajuste bloqueado.`);
+        } else {
+            this._temp = temp;
+        }
+    };
+}
+
+const statusServidor = {
+    ligado: true,
+    exibir: () => {
+        console.log(`Servidor ativo: ${statusServidor.ligado ? 'sim' : 'não'}`);
+    }
+};
+
+const servidor1 = new DataCenter();
+servidor1.temp = 45;
+servidor1.temp = 30;
+servidor1.temp = 5;
+statusServidor.exibir();
+console.log(`Temperatura atual do sistema: ${servidor1.temp}°C`);
